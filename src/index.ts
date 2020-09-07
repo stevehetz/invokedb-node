a92aff5c77189a7b84ac7accff419e647aefdb0a
Steve Hetzel <stevehetzel2@gmail.com>
2020-09-07 14:15:40 -0400

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-import foo from './foo.js';
+import axios from 'axios';
+
+const config = {
+  BASE_URL: 'https://api.invokedb.com/v1',
+  API_KEY: 'fmh99QgCYnufhYDuAYSgpPblGAYgEWi7'
+};
+
+const getPaging = async () => {
+  const table = 'contacts';
+  const limit = 10;
+  const headers = { Authorization: `Bearer ${config.API_KEY}` };
+
+  let url = `https://api.invokedb.com/v1/get?table=${table}`;
+
+  let skip = 0;
+  url += `&skip=${skip}&limit=${limit}`;
+  let res = await axios.get(url, { headers });
+  console.log(res);
+  // { data: { count: 200, data: [...] } }
+
+  skip = skip + limit;
+  url += `&skip=${skip}&limit=${limit}`;
+  res = await axios.get(url, { headers });
+  console.log(res);
+  // { data: { count: 200, data: [...] } }
+};
+
 
 export const invokedb = function () {
-  console.log(foo);
+  getPaging();
 }
