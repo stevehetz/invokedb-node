d252bf23a4b855ad8acfb1e1f9cde661b8c7cb1c
Steve Hetzel <stevehetzel2@gmail.com>
2020-09-07 11:37:40 -0400

    setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,29 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
 export default {
-  input: 'src/main.js',
-  output: {
-    name: 'test',
-    file: 'bundle.js',
-    format: 'umd',
-  },
+  input: 'src/index.ts', // our source file
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    },
+    {
+      file: pkg.browser,
+      format: 'iife',
+      name: 'invokedb'
+    }
+  ],
+  external: [...Object.keys(pkg.dependencies || {})],
+  plugins: [
+    typescript({
+      typescript: require('typescript')
+    }),
+    // terser()
+  ]
 };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import foo from './foo.js';
+
+export const invokedb = function () {
+  console.log(foo);
+}
