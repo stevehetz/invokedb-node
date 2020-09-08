a1189286d498d602769e17dad1d07d9316f0d045
Steve Hetzel <stevehetzel2@gmail.com>
2020-09-08 15:27:43 -0400

    setup

--- src/index.ts
+++ src/index.ts
@@ -13,36 +13,69 @@ export interface ISortParam {
 export interface IGetParams {
   skip?: number;
   limit?: number;
-  sort?: ISortParam;
+  sortBy?: string;
+  sortDir?: SORT_DIR;
   filter?: any;
 }
 
+const DEFAULT_LIMIT = 10;
+
 
 class InvokeDBTableClient {
-  constructor(private _baseUrl: string, private _apiKey: string, private _tableName: string) {}
+  constructor(private _baseUrl: string, private _apiKey: string, private _tableName: string) { }
+  
+  async findOne(filter?: any) {
+    const res = await this.find({ filter });
+    if (res && res.data && res.data.data && res.data.data[0]) {
+      return res.data.data[0];
+    }
 
-  async get(params: IGetParams) {
-    const { skip, limit, sort, filter } = params;
+    return undefined;
+  }
+
+  async find(params?: IGetParams) {
+    params = params || {};
+    params.limit = typeof params.limit === 'number' ? params.limit : DEFAULT_LIMIT;
+    params.skip = typeof params.skip === 'number' ? params.skip : 0;
+    const { skip, limit, sortBy, sortDir, filter } = params;
     const headers = { Authorization: `Bearer ${this._apiKey}` };
 
     let urlQuery = `table=${this._tableName}`;
     urlQuery += `&skip=${skip}`;
     urlQuery += `&limit=${limit}`;
 
-    if (sort) {
-      urlQuery += `&sort_by=${sort.sortBy}`;
-      urlQuery += `&sort_dir=${sort.sortDir}`;
+    if (sortBy) {
+      urlQuery += `&sort_by=${sortBy}`;
+      const sort_dir = sortDir || 'asc';
+      urlQuery += `&sort_dir=${sort_dir}`;
     }
 
     return filter
       ? await axios.post(`${this._baseUrl}/search?${urlQuery}`, filter, { headers })
       : await axios.get(`${this._baseUrl}/get?${urlQuery}`, { headers });
   }
+
+  async count() {
+
+  }
+
+  async insert() {
+
+  }
+
+  async update() {
+
+  }
+
+  async delete() {
+
+  }
 }
 
 export class InvokeDBClient {
-  private _baseUrl;
-  private _apiKey;
+  private _baseUrl: string;
+  private _apiKey: string;
+  
   constructor(private _config: any) {
     const { baseUrl, apiKey } = this._config;
     if (!apiKey && typeof apiKey !== 'string') {
